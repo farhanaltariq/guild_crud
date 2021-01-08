6971e89325ba202196b10385a596107bc2506734
hanpetualang <farhanaaxy@gmail.com>
2021-01-08 20:52:07 +0700

    Update guild.sql

--- guild.sql
+++ guild.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Jan 2021 pada 10.41
+-- Waktu pembuatan: 08 Jan 2021 pada 14.51
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.11
 
@@ -20,6 +20,47 @@ SET time_zone = "+00:00";
 --
 -- Database: `guild`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `goods`
+--
+
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `rank` varchar(2) NOT NULL,
+  `value` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `goods`
+--
+
+INSERT INTO `goods` (`id`, `name`, `rank`, `value`) VALUES
+(1, 'Thundersoother Diadem', 'S', 100),
+(2, 'Royal Flora', 'A', 55);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `herbs`
+--
+
+CREATE TABLE `herbs` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `rank` varchar(2) NOT NULL,
+  `value` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `herbs`
+--
+
+INSERT INTO `herbs` (`id`, `name`, `rank`, `value`) VALUES
+(1, 'Mint', 'C', 0.05);
 
 -- --------------------------------------------------------
 
@@ -44,6 +85,27 @@ INSERT INTO `hunter` (`id`, `name`, `rank`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `monster`
+--
+
+CREATE TABLE `monster` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `rank` varchar(2) NOT NULL,
+  `rarity` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `monster`
+--
+
+INSERT INTO `monster` (`id`, `name`, `rank`, `rarity`) VALUES
+(1, 'Geovishap Hatchling', 'A', '210'),
+(2, 'Dvalin Dragon', 'S', '310');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `quest`
 --
 
@@ -63,8 +125,10 @@ CREATE TABLE `quest` (
 --
 
 INSERT INTO `quest` (`id`, `title`, `rank`, `type`, `maxhunter`, `minpower`, `reward`, `availability`) VALUES
-(2, 'Escort Goods', 'C', 'Escort', 1, 20, 0.05, 'Available'),
-(12, 'Hunt Shark', 'S', 'Subjugation', 5, 70, 10, 'Available');
+(2, 'Escort Goods', 'C', 'E  s c o r  t', 1, 20, 0.05, 'Available'),
+(12, 'Hunt Shark', 'S', 'Subjugation', 5, 70, 10, 'Available'),
+(14, 'Kill Goblin', 'C', 'Subjugation', 1, 30, 0.5, 'Available'),
+(15, 'Escort Traveler', 'B', 'E  s c o r  t', 3, 60, 0.5, 'Available');
 
 -- --------------------------------------------------------
 
@@ -92,9 +156,27 @@ INSERT INTO `users` (`id`, `name`, `username`, `password`) VALUES
 --
 
 --
+-- Indeks untuk tabel `goods`
+--
+ALTER TABLE `goods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `herbs`
+--
+ALTER TABLE `herbs`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `hunter`
 --
 ALTER TABLE `hunter`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `monster`
+--
+ALTER TABLE `monster`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -114,10 +196,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `goods`
+--
+ALTER TABLE `goods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `herbs`
+--
+ALTER TABLE `herbs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT untuk tabel `hunter`
 --
 ALTER TABLE `hunter`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `monster`
+--
+ALTER TABLE `monster`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `quest`
+--
+ALTER TABLE `quest`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
